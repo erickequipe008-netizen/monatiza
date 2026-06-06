84f5b1ddbaed492caa04c9cd4ce3a4eb1f8af9e0
adm <adm@MacBook-Pro.local>
2026-06-06 10:56:21 -0300

    test header

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,72 +2,19 @@
 
 export default function Header() {
   return (
-    <>
-
-      {/* TOP BRANDS */}
-      <div
-        className="
-          w-full
-          h-[42px]
-          bg-white
-          border-b
-          border-zinc-200
-          flex
-          items-center
-          gap-6
-          px-5
-          overflow-x-auto
-          whitespace-nowrap
-          text-black
-          text-[14px]
-          font-bold
-        "
-      >
-
-        <a href="/">monatiza</a>
-
-        <a href="/brazil">
-          monatiza brazil
-        </a>
-
-        <a href="/play">
-          monatiza play
-        </a>
-
-        <a href="/esportes">
-          monatiza esportes
-        </a>
-
-        <a href="/saude">
-          monatiza saúde
-        </a>
-
-        <a href="/life">
-          monatiza life
-        </a>
-
-        <a href="/empreende">
-          empreende
-        </a>
-
-      </div>
-
-      {/* HEADER */}
-      <header
-        className="
-          sticky top-0 z-40
-          bg-black text-white
-        "
-      >
-        <div className="max-w-[1600px] mx-auto h-[78px] px-5 flex items-center justify-between">
-
-          <h1 className="text-3xl font-bold">
-            MONATIZA
-          </h1>
-
-        </div>
-      </header>
-
-    </>
+    <div
+      style={{
+        background: "red",
+        height: "120px",
+        color: "white",
+        fontSize: "40px",
+        fontWeight: "bold",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      HEADER TESTE
+    </div>
   );
 }
