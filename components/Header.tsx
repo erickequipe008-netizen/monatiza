422bf9ee6bbd1f1487051b5ca35298bd3aff1b02
adm <adm@MacBook-Pro.local>
2026-06-06 10:50:03 -0300

    test red bar

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,8 +2,72 @@
 
 export default function Header() {
   return (
-    <header className="bg-black text-white p-6">
-      <h1>MONATIZA</h1>
-    </header>
+    <>
+
+      {/* TOP BRANDS */}
+      <div
+        className="
+          w-full
+          h-[42px]
+          bg-white
+          border-b
+          border-zinc-200
+          flex
+          items-center
+          gap-6
+          px-5
+          overflow-x-auto
+          whitespace-nowrap
+          text-black
+          text-[14px]
+          font-bold
+        "
+      >
+
+        <a href="/">monatiza</a>
+
+        <a href="/brazil">
+          monatiza brazil
+        </a>
+
+        <a href="/play">
+          monatiza play
+        </a>
+
+        <a href="/esportes">
+          monatiza esportes
+        </a>
+
+        <a href="/saude">
+          monatiza saúde
+        </a>
+
+        <a href="/life">
+          monatiza life
+        </a>
+
+        <a href="/empreende">
+          empreende
+        </a>
+
+      </div>
+
+      {/* HEADER */}
+      <header
+        className="
+          sticky top-0 z-40
+          bg-black text-white
+        "
+      >
+        <div className="max-w-[1600px] mx-auto h-[78px] px-5 flex items-center justify-between">
+
+          <h1 className="text-3xl font-bold">
+            MONATIZA
+          </h1>
+
+        </div>
+      </header>
+
+    </>
   );
 }
