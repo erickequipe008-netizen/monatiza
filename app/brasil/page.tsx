ba07b18a5d932fdd74638064febb323f96a16e86
adm <adm@MacBook-Pro.local>
2026-06-10 17:52:07 -0300

    force update

---
+++ app/brasil/page.tsx
@@ -0,0 +1,14 @@
+
+export const metadata = {
+  title: "Brasil | Monatiza",
+  description: "Últimas notícias do Brasil.",
+};
+
+export default function BrasilPage() {
+  return (
+    <main className="p-10">
+      <h1>Brasil</h1>
+    </main>
+  );
+}
+
