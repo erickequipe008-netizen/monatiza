43fd03022bb137ad40ce664cad3d7d56f788e4d1
adm <adm@MacBook-Pro.local>
2026-06-22 19:04:59 -0300

    teste footer

---
+++ components/Footer.tsx
@@ -0,0 +1,15 @@
+export default function Footer() {
+  return (
+    <footer
+      style={{
+        background: "#000",
+        color: "#fff",
+        padding: "40px",
+        textAlign: "center",
+      }}
+    >
+      <h2>MONATIZA</h2>
+      <p>Rodapé de teste</p>
+    </footer>
+  );
+}
